50ec9af0061fc4aa33f055aa73c8219f81cfc581
vstartech11 <11211024@student.itk.ac.id>
2022-07-07 20:38:16 +0800

    update landing

--- application/views/landing/index.php
+++ application/views/landing/index.php
@@ -22,7 +22,7 @@
                             <div class="btn-wrapper mt-4">
                                 <a href="https://www.creative-tim.com/product/argon-design-system" class="btn btn-warning btn-icon mt-3 mb-sm-0">
                                     <span class="btn-inner--icon"><i class="ni ni-button-play"></i></span>
-                                    <span class="btn-inner--text">Play more</span>
+                                    <span class="btn-inner--text">Lihat Desain</span>
                                 </a>
                             </div>
                         </div>
@@ -42,28 +42,15 @@
                 <div class="col-lg-6">
                     <div class="info info-horizontal info-hover-primary">
                         <div class="description pl-4">
-                            <h5 class="title">For Developers</h5>
-                            <p>The time is now for it to be okay to be great. People in this world shun people for being great. For being a bright color. For standing out. But the time is now.</p>
-                            <a href="#" class="text-info">Learn more</a>
+                            <h5 class="title">Definisi website</h5>
+                            <p>Penjelasan singkat.</p>
+                            <a href="<?= base_url('landing/tentang'); ?>" class="text-info">Pelajari Lebih Lanjut</a>
                         </div>
                     </div>
-                    <div class="info info-horizontal info-hover-primary mt-5">
-                        <div class="description pl-4">
-                            <h5 class="title">For Designers</h5>
-                            <p>There’s nothing I really wanted to do in life that I wasn’t able to get good at. That’s my skill. I’m not really specifically talented at anything except for the ability to learn.</p>
-                            <a href="#" class="text-info">Learn more</a>
-                        </div>
-                    </div>
-                    <div class="info info-horizontal info-hover-primary mt-5">
-                        <div class="description pl-4">
-                            <h5 class="title">For Beginners</h5>
-                            <p>That’s what I do. That’s what I’m here for. Don’t be afraid to be wrong because you can’t learn anything from a compliment. If everything I did failed - which it doesn't.</p>
-                            <a href="#" class="text-info">Learn more</a>
-                        </div>
-                    </div>
+
                 </div>
                 <div class="col-lg-6 col-10 mx-md-auto">
-                    <img class="ml-lg-5" src="../assets/img/ill/ill.png" width="100%">
+                    <img class="ml-lg-5" src="<?= base_url('assets/'); ?>img/ill/ill.png" width="100%">
                 </div>
             </div>
         </div>
